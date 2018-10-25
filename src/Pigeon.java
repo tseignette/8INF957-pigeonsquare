1801342d6d5c15eebb29c34729218510b456cbb4
Thomas Seignette <tseignette@enseirb-matmeca.fr>
2018-10-25 19:04:05 -0400

    Code réarrangé et documenté

--- src/Pigeon.java
+++ src/Pigeon.java
@@ -31,27 +31,12 @@ public class Pigeon extends Drawing implements Runnable {
   }
 
   // ===============================================================================================
-  // FUNCTIONS
+  // PRIVATE FUNCTIONS
   // ===============================================================================================
-  public Thread getThread() {
-    return this.thread;
+  private boolean isNear(Point2D foodPos) {
+    return(this.getPos().distance(foodPos) < 2);
   }
 
-  public Point2D getPos() {
-    return new Point2D(getView().getX(), getView().getY());
-  }
-
-  public void updatePigeon(Point2D pos) {
-    double newX = (pos.getX() > 0 && pos.getX() < SquareDisplay.WINDOWS_WIDTH) ? pos.getX() : getPos().getX();
-    double newY = (pos.getY() > 0 && pos.getY() < SquareDisplay.WINDOWS_HEIGHT) ? pos.getY() : getPos().getY();
-
-    setPosition(new Point2D(newX, newY));
-  }
-
-  public Point2D getVector(Point2D depart, Point2D arrivee) {
-    return new Point2D(arrivee.getX() - depart.getX(), arrivee.getY() - depart.getY());
-  }
-  
   private Food getNearest(List<Food> list) {
     Food sol = list.get(0);
     
@@ -68,20 +53,38 @@ public class Pigeon extends Drawing implements Runnable {
 	  return sol;
   }
 
-  private boolean isNear(Point2D foodPos) {
-    return(this.getPos().distance(foodPos) < 2);
+  private Point2D getVector(Point2D depart, Point2D arrivee) {
+    return new Point2D(arrivee.getX() - depart.getX(), arrivee.getY() - depart.getY());
   }
 
-  public void move(Point2D arrivee) {
+  private void updatePigeon(Point2D pos) {
+    double newX = (pos.getX() > 0 && pos.getX() < SquareDisplay.WINDOWS_WIDTH) ? pos.getX() : getPos().getX();
+    double newY = (pos.getY() > 0 && pos.getY() < SquareDisplay.WINDOWS_HEIGHT) ? pos.getY() : getPos().getY();
+
+    setPosition(new Point2D(newX, newY));
+  }
+
+  private void move(Point2D arrivee) {
     Point2D vector = getVector(this.getPos(), arrivee).normalize();
     Point2D newPos = this.getPos().add(vector.multiply(SPEED));
     updatePigeon(newPos);
   }
 
-  public void fear(Point2D originFear) {
+  private void fear(Point2D originFear) {
 	  Point2D vector = getVector(originFear, this.getPos()).normalize();
 	  Point2D newPos = this.getPos().add(vector.multiply(SPEED));
 	  updatePigeon(newPos);
+  }
+
+  // ===============================================================================================
+  // PUBLIC FUNCTIONS
+  // ===============================================================================================
+  public Thread getThread() {
+    return this.thread;
+  }
+
+  public Point2D getPos() {
+    return new Point2D(getView().getX(), getView().getY());
   }
 
   public void run() {
@@ -89,19 +92,25 @@ public class Pigeon extends Drawing implements Runnable {
     try {
       while(!thread.isInterrupted()) {
         try {
+          // On attend des infos du square
           synchronized(square) {
             square.wait();
           }
 
+          // Tant qu'il y a un bad guy ou de la nourriture
           while(square.hasScary() || square.hasFood()) {
+            // Si bad guy, on fuit
             if(square.hasScary()) {
               fear(square.getScary());
             }
+            // Si nourriture, on va vers la plus proche
             else if(square.hasFood()) {
-              ArrayList<Food> foodList = square.getFood();
               try {
+                ArrayList<Food> foodList = square.getFood();
                 Food nearestFood = getNearest(foodList);
                 move(nearestFood.getPos());
+
+                // Si elle est assez proche, on essaie de la manger
                 if(isNear(nearestFood.getPos())) square.eatFood(nearestFood);
               }
               finally { }
--- src/SquareDisplay.java
+++ src/SquareDisplay.java
@@ -23,31 +23,103 @@ public class SquareDisplay extends Application {
 	// ================================================================================================
 	// ATTRIBUTES
 	// ================================================================================================
+	private SquareDisplay me = this;
+	private ThreadGroup threadGroup;
 	private Group root;
 	private ArrayList<Pigeon> pigeonList = new ArrayList<Pigeon>();
-	private ThreadGroup threadGroup;
-	private boolean hasScary = false;
-	private Point2D scaryPos;
 	private ArrayList<Food> foodList = new ArrayList<Food>();
-	private SquareDisplay me = this;
+	private Point2D scaryPos;
 
 	// ===============================================================================================
-	// FUNCTIONS
+	// PRIVATE FUNCTIONS
+	// ===============================================================================================
+	private void sceneBuilder(Stage primaryStage) {
+		Scene scene = new Scene(
+			this.root,
+			SquareDisplay.WINDOWS_WIDTH,
+			SquareDisplay.WINDOWS_HEIGHT,
+			SquareDisplay.WINDOW_BACKGROUND_COLOR
+		);
+	  
+		primaryStage.setTitle("Pigeon Square");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.getIcons().add(new Image("./pigeon.png"));
+	}
+  
+	private void pigeonsBuilder(int pigeonNb) {
+		this.threadGroup = new ThreadGroup("Pigeons");
+		
+		for(int i = 0; i < pigeonNb; i++) {
+			Pigeon pigeon = new Pigeon(this.threadGroup, root, this);
+			pigeon.setDrawingPosition(Utils.randomPoint2D())
+			.draw();
+
+			this.pigeonList.add(pigeon);
+		}
+	}
+  
+	private void spawnScary(Point2D mousePos) {
+		if(!hasScary()) {
+			scaryPos = mousePos;
+
+			BadGuy scary = new BadGuy(root, me);
+			scary.setDrawingPosition(scaryPos)
+			.draw();
+
+			synchronized(me) {
+				me.notifyAll();
+			}
+		}
+	}
+	
+	private void spawnFood(Point2D mousePos) {
+		Food food = new Food(root, me);
+		food.setDrawingPosition(mousePos)
+		.draw();
+
+		foodList.add(food);
+
+		synchronized(me) {
+			me.notifyAll();
+		}
+	}
+	
+	private void startPigeons() {
+		for(Pigeon pigeon: this.pigeonList) {
+			pigeon.getThread().start();
+		}
+	}
+  
+	private void stopPigeons() {
+		this.threadGroup.interrupt();
+	}
+
+	// ===============================================================================================
+	// PUBLIC FUNCTIONS
 	// ===============================================================================================
 	public boolean hasFood() {
 		return !foodList.isEmpty();
 	}
 
 	public boolean hasScary() {
-		return hasScary;
+		return scaryPos != null;
 	}
 	
 	public ArrayList<Food> getFood() {
 		return foodList;
 	}
 
+	public Point2D getScary() {
+		return scaryPos;
+	}
+
 	public void removeFood(Food food) {
 		foodList.remove(food);
+	}
+
+	public void removeScary() {
+		scaryPos = null;
 	}
 
 	public boolean eatFood(Food food) {
@@ -61,14 +133,6 @@ public class SquareDisplay extends Application {
 		return ret;
 	}
 
-	public Point2D getScary() {
-		return scaryPos;
-	}
-
-	public void removeScary() {
-		hasScary = false;
-	}
-
 	public void start(Stage primaryStage) {
 		this.root = new Group();
 		this.foodList = new ArrayList<Food>();
@@ -76,16 +140,20 @@ public class SquareDisplay extends Application {
 		sceneBuilder(primaryStage);
 		pigeonsBuilder(10);
 		
+		// On arrête les threads à la fermeture de la fenêtre
 		primaryStage.setOnCloseRequest(event -> stopPigeons());
 		
+		// On gère le clic gauche et droit
 		primaryStage.getScene().addEventFilter(MouseEvent.MOUSE_RELEASED, new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
 				Point2D mousePos = new Point2D(mouseEvent.getX(), mouseEvent.getY());
 
+				// Clic gauche -> nourriture
 				if(mouseEvent.getButton() == MouseButton.PRIMARY) {
 					spawnFood(mousePos);
 				}
+				// Clic droite -> bad guy
 				else if(mouseEvent.getButton() == MouseButton.SECONDARY) {
 					spawnScary(mousePos);
 				}
@@ -94,70 +162,6 @@ public class SquareDisplay extends Application {
 
 		primaryStage.show();
 		startPigeons();
-	}
-
-	public void sceneBuilder(Stage primaryStage) {
-		Scene scene = new Scene(
-			this.root,
-			SquareDisplay.WINDOWS_WIDTH,
-			SquareDisplay.WINDOWS_HEIGHT,
-			SquareDisplay.WINDOW_BACKGROUND_COLOR
-		);
-	  
-		primaryStage.setTitle("Pigeon Square");
-		primaryStage.setScene(scene);
-		primaryStage.setScene(scene);
-		primaryStage.setResizable(false);
-		primaryStage.getIcons().add(new Image("./pigeon.png"));
-	}
-  
-	public void pigeonsBuilder(int pigeonNb) {
-		this.threadGroup = new ThreadGroup("Pigeons");
-		
-		for(int i = 0; i < pigeonNb; i++) {
-			Pigeon pigeon = new Pigeon(this.threadGroup, root, this);
-			pigeon.setDrawingPosition(Utils.randomPoint2D())
-			.draw();
-
-			this.pigeonList.add(pigeon);
-		}
-	}
-  
-	public void spawnScary(Point2D mousePos) {
-		if(!hasScary) {
-			hasScary = true;
-			scaryPos = mousePos;
-
-			BadGuy scary = new BadGuy(root, me);
-			scary.setDrawingPosition(scaryPos)
-			.draw();
-
-			synchronized(me) {
-				me.notifyAll();
-			}
-		}
-	}
-	
-	public void spawnFood(Point2D mousePos) {
-		Food food = new Food(root, me);
-		food.setDrawingPosition(mousePos)
-		.draw();
-
-		foodList.add(food);
-
-		synchronized(me) {
-			me.notifyAll();
-		}
-	}
-	
-	public void startPigeons() {
-		for(Pigeon pigeon: this.pigeonList) {
-			pigeon.getThread().start();
-		}
-	}
-  
-	public void stopPigeons() {
-		this.threadGroup.interrupt();
 	}
   
 }
